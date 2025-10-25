0ab0de8857c836bdb11bcebf6f503eb1e051eca8
SHOIYAN <shoiyan9@gmail.com>
2025-10-25 17:28:16 +0300

    feat: add Book constructor

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function Book (title,author,pages,haveRead) {
+    if (!new.target) {
+        throw Error ("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages,${this.haveRead}`
+    }
+
+}
+
+const theHobbit = new Book ('Hobbit','John',4000,'not read yet');
+
+console.log(theHobbit.info());
